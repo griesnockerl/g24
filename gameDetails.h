37708a0374a2c67daf6709bb6cb4fec4a7b0f3a4
griesnockerl <griesnockerl@users.noreply.github.com>
2014-12-20 13:55:33 +0100

    Update gameDetails.h

--- gameDetails.h
+++ gameDetails.h
@@ -1,8 +1,12 @@
-#ifndef gameDetails
+ifndef gameDetails
 #define gameDetails
 
 static char *playerName;
 static int playerCount;
 static int playerNumber;
+static int piecesCount;
+static int maxTimeMove;
+static int remainToHit;
+
 
 #endif
